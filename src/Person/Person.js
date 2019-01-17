aee5832dcf7c5c79ba6d9d0e1e5a1e51f2bea1eb
pedrobarata <pedrofelipebarata@gmail.com>
2019-01-17 20:53:00 -0200

    feat(3.37): Understanding the Children Property

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const person = (props) => {
     return (
+        <div>
         <p>I'm {props.name} and I have {props.age} years old!</p>
+        <p>{props.children}</p>
+        </div>
     );
 }
 
