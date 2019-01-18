92602c05470407c2b19d42e77c128f18499831ba
pedrobarata <pedrofelipebarata@gmail.com>
2019-01-17 22:43:39 -0200

    feat(3.47): Working with Inline Styles

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    persons: [
-      {name: "Pedro", age: 25},
-      {name: "Test", age: 30}
-    ]
+    persons: [{ name: "Pedro", age: 25 }, { name: "Test", age: 30 }]
   };
 
-  switchNameHandler = (myNewName) => {
+  switchNameHandler = myNewName => {
     this.setState({
-      persons: [
-        {name: myNewName, age: 25},
-        {name: "Test", age: 40}
-      ]
+      persons: [{ name: myNewName, age: 25 }, { name: "Test", age: 40 }]
     });
-  }
+  };
 
-  nameChangedHander = (event) => {
+  nameChangedHander = event => {
     this.setState({
       persons: [
-        {name: "Pedro", age: 25},
-        {name: event.target.value, age: 40}
+        { name: "Pedro", age: 25 },
+        { name: event.target.value, age: 40 }
       ]
     });
-  }
+  };
 
   render() {
+    const buttonStyle = {
+      backgroundColor: "white",
+      border: "1px solid red",
+      font: "inherit",
+      padding: "10px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>Hi!</h1>
         {/* Arrow Functions here could be inefficient!!! */}
-        <button onClick={() => this.switchNameHandler('Pedrooo!')}>Change name!</button>
+        <button style={buttonStyle} onClick={() => this.switchNameHandler("Pedrooo!")}>
+          Change name!
+        </button>
         <Person
-        click={this.switchNameHandler.bind(this, 'Pedroooww!')} 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}/>
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        changed={this.nameChangedHander}>My hobbies: Racing </Person>
+          click={this.switchNameHandler.bind(this, "Pedroooww!")}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHander}
+        >
+          My hobbies: Racing{" "}
+        </Person>
       </div>
     );
   }
