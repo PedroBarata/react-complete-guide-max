3107c52ada7e6df44721ad32b38ccf25403ec961
pedrobarata <pedrofelipebarata@gmail.com>
2019-01-17 22:33:40 -0200

    feat(3.46): Adding Style with Stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import './Person.css';
 const person = (props) => {
     /* Vai dar um erro, "falso alarme". Pois o primeiro valor de Person, no App.js não
         tem o método criado do onChange sendo passado como referência (por isso, não
         conseguimos mudar o valor do primeiro campo, inclusive). Este erro, é apenas
         para lembrar que a propriedade "value" não poderá ser alterada! */
     return (
-        <div>
+        <div className="Person">
         <p onClick={props.click}>I'm {props.name} and I have {props.age} years old!</p>
         <p>{props.children}</p>
         <input onChange={props.changed} value={props.name}/>
