a590243913ad0ebe0dab5b372fdba62c723407f5
pedrobarata <pedrofelipebarata@gmail.com>
2019-01-30 18:37:40 -0200

    feat(7.85): componentWillUnmount

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,27 +4,35 @@ import classes from "./Person.css";
 class Person extends Component {
   constructor(props) {
     super(props);
-    console.log('[Person.js] Inside constructor()', props);
+    console.log("[Person.js] Inside constructor()", props);
   }
 
   componentWillMount() {
-    console.log('[Person.js] Inside componentWillMount()');
+    console.log("[Person.js] Inside componentWillMount()");
   }
 
   componentDidMount() {
-    console.log('[Person.js] Inside componentDidMount()');
+    console.log("[Person.js] Inside componentDidMount()");
   }
-  
+
+  componentWillUnmount() {
+    // Component is about to get removed => Perform any cleanup work here!
+    console.log("[Person.js] I'm about to be removed!");
+  }
 
   render() {
-    console.log('[Person.js] Inside render()');
+    console.log("[Person.js] Inside render()");
     return (
       <div className={classes.Person}>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I have {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
       </div>
     );
   }
