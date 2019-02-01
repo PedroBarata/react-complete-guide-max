5911681100dd9eb0248090854e4b7845345c6846
pedrobarata <pedrofelipebarata@gmail.com>
2019-02-01 13:26:16 -0200

    feat(7.94): Returning Adjacent Elements

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Cockpit.css";
+import Aux from "../../hoc/Aux";
 
 const cockpit = props => {
-  let btnClass = "";
+  let btnClass = classes.Button;
   const objectClassesCss = [];
 
-  if(props.showPersons) {
-    btnClass = classes.Red;
+  if (props.showPersons) {
+    btnClass = [classes.Button, classes.Red].join(" ");
   }
 
   if (props.personsLength <= 1) {
@@ -16,16 +17,18 @@ const cockpit = props => {
     objectClassesCss.push(classes.bold); //classes = ['red', 'bold'];
   }
 
-  
   return (
-    <div className={classes.Cockpit}>
+    /* A partir do React 16.2, 
+    pode-se passar uma tag vazia, 
+    que faz o mesmo trabalho do Aux */
+    <>
       <h1>Hi!</h1>
       <p className={objectClassesCss.join(" ")}>Test with dynamic classes!</p>
       {/* Arrow Functions here could be inefficient!!! */}
       <button className={btnClass} onClick={props.clicked}>
         Toggle Persons!
       </button>
-    </div>
+    </>
   );
 };
 
