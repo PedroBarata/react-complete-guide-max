4afc3a24267e7ccd4e1fa5b59bff2eda64391e38
pedrobarata <pedrofelipebarata@gmail.com>
2019-02-05 14:52:23 -0200

    feat(7.103): More on the React ref API (16.3) Novo feature de ref no react 16.3+

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,7 @@ class Person extends Component {
   constructor(props) {
     super(props);
     console.log("[Person.js] Inside constructor()", props);
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -16,7 +17,7 @@ class Person extends Component {
   componentDidMount() {
     console.log("[Person.js] Inside componentDidMount()");
     if (this.props.position === 0) {
-      this.inputElement.focus();
+      this.inputElement.current.focus();
     }
   }
 
@@ -35,7 +36,7 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input
           type="text"
-          ref={input => (this.inputElement = input)}
+          ref={this.inputElement}
           onChange={this.props.changed}
           value={this.props.name}
         />
